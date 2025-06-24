a0f62af64015015f822bcee432145b0599e1e7ac
Michalis Giannoudis <michalis221@gmail.com>
2025-06-24 15:03:28 +0300

    finished language select on all media querries

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -9,11 +9,11 @@ import { Language } from '@/types/language.interface';
 export default function Header() {
 
   const languageState = useThemeStore(state => state.language);
-  const [openMenu, setOpenMenu] = useState<boolean>(false);
+  const [ openMenu, setOpenMenu ] = useState<boolean>(false);
   const { homeOption, servicesOption, contactOption } = useContent(languageState === Language.Greek ? 'headerGR' : 'headerEN') as HeaderContent;
 
   return (
-    <div className="grid grid-cols-[80%_20%] md:grid-cols-[35%_25%_15%_25%] lg:grid-cols-[50%_20%_15%_15%] text-center bg-gray-900 w-full h-auto">
+    <div className="grid grid-cols-[72%_13%_15%] md:grid-cols-[35%_25%_15%_25%] lg:grid-cols-[50%_20%_15%_15%] text-center bg-gray-900 w-full h-auto">
       <div>
         <img src="/logo-cropped.png" className="w-50 h-auto p-5"/>
       </div>
@@ -38,14 +38,14 @@ export default function Header() {
         <img src="/phone-icon-2.png" className="w-7 h-7 justify-self-end animate-pulse"/>
         <p className="text-white text-base justify-self-start cursor-pointer">6912345678</p>
       </div>
-      <div className="hidden md:grid justify-items-center items-center">
-        <LanguageMenu />
+      <div className="grid justify-items-center items-center">
+        <LanguageMenu openMenu={openMenu} setOpenMenu={setOpenMenu}/>
       </div>
       <div className="mt-auto mb-auto flex justify-center md:hidden">
         <img src={openMenu ? "/menu-close.png" : "/menu-open.png"} className={`w-8 h-8 transition-transform duration-300 ${openMenu ? 'rotate-180' : 'rotate-0'}`} onClick={() => setOpenMenu(!openMenu)}/>
       </div>
       {openMenu && 
-      <div className="absolute mt-21 mr-5 right-0 bg-gray-800 text-white w-48 p-4 rounded-md shadow-lg">
+      <div className="absolute mt-22 mr-5 right-0 bg-gray-700 text-white w-48 p-4 rounded-md shadow-lg md:hidden">
         <div>
           <a href="/" className="block px-4 py-2 hover:bg-gray-700">{homeOption}</a>
           <a href="/" className="block px-4 py-2 hover:bg-gray-700">{servicesOption}</a>
--- src/components/languageMenu.component.tsx
+++ src/components/languageMenu.component.tsx
@@ -1,47 +1,78 @@
 'use client';
-import { useState } from 'react';
+
 import { useThemeStore } from '@/store/theme.store';
 import { Language } from '@/types/language.interface';
 
-export const LanguageMenu = () => {
+export const LanguageMenu = ({openMenu, setOpenMenu}: {openMenu:boolean, setOpenMenu: (value:boolean) => void}) => {
 
-  const [open, setOpen] = useState(false);
   const languageState = useThemeStore(state => state.language);
   const setStoreLanguage = useThemeStore(state => state.setLanguage);
 
   const  toggleLanguage = (language: Language) => {
     setStoreLanguage(language);
-    setOpen(false);
+    setOpenMenu(false)
   }
 
   return (
     <div className="relative flex items-center justify-self-center">
-      <button
-        type="button"
-        onClick={() => setOpen(!open)}
-        className="inline-flex items-center font-medium px-4 py-2 text-sm text-gray-900 dark:text-white rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
-      >
-        <img src={languageState === Language.Greek ? '/greece-flag.png' : '/american-flag.png'} className="w-5 h-5 rounded-full me-3" alt="Greek Flag"/>
-        {languageState === Language.Greek ? 'Ελληνικά (GR)' : 'English (US)'}
-      </button>
+      
+      <div className='hidden md:flex'>
+        <button
+          type="button"
+          onClick={() => { setOpenMenu(!openMenu) }}
+          className="inline-flex items-center font-medium px-4 py-2 text-sm text-gray-900 dark:text-white rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
+        >
+          <img src={languageState === Language.Greek ? '/greece-flag.png' : '/american-flag.png'} className="w-5 h-5 rounded-full me-3" alt="Greek Flag"/>
+          {languageState === Language.Greek ? 'Ελληνικά (GR)' : 'English (US)'}
+        </button>
 
-      {open && (
-        <div className="absolute right-0 mt-35 w-48 bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700">
-          <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
-            <li>
-              <button onClick={() => toggleLanguage(Language.English)} className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-                English (US)
-              </button>
-            </li>
+        {!openMenu && (
+          <div className="absolute right-0 mt-16 w-48 bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700">
+            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+              <li>
+                <button onClick={() => toggleLanguage(Language.English)} className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+                  {Language.English}
+                </button>
+              </li>
 
-            <li>
-              <button onClick={() => toggleLanguage(Language.Greek)} className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-                Ελληνικά (GR)
-              </button>
-            </li>
-          </ul>
+              <li>
+                <button onClick={() => toggleLanguage(Language.Greek)} className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+                  {Language.Greek}
+                </button>
+              </li>
+            </ul>
+          </div>
+        )}
         </div>
-      )}
+        
+        <div  className='flex md:hidden'>
+          <button
+            type="button"
+            onClick={() => setOpenMenu(!openMenu)}
+            className="inline-flex items-center font-medium px-4 py-2 text-sm text-gray-900 dark:text-white rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
+          >
+            <img src={languageState === Language.Greek ? '/greece-flag.png' : '/american-flag.png'} className="w-5 h-5 rounded-full me-3" alt="Greek Flag"/>
+          </button>
+
+          {!openMenu && (
+            <div className="absolute right-0 mt-16 w-48 bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700">
+              <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+                <li>
+                  <button onClick={() => toggleLanguage(Language.English)} className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+                    {Language.English}
+                  </button>
+                </li>
+
+                <li>
+                  <button onClick={() => toggleLanguage(Language.Greek)} className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+                    {Language.Greek}
+                  </button>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+
     </div>
   );
 };
