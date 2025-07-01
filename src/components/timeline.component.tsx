5570b944a23a84d0a69f8a7d2b02caa96fe745a0
Michalis Giannoudis <michalis221@gmail.com>
2025-07-01 17:25:32 +0300

    fixed timeline for tablet

--- src/components/timeline.component.tsx
+++ src/components/timeline.component.tsx
@@ -12,13 +12,13 @@ export const TimeLine = () => {
     return (    
     
     <div>
-        <div className="hidden md:block relative mx-auto py-16 px-4 w-[75%]">
+        <div className="hidden lg:block relative mx-auto py-16 px-4 w-[75%]">
             <div className="absolute top-1/2 left-4 right-4 h-1 bg-gradient-to-r from-white to-gray-400 rounded-full transform -translate-y-1/2 z-10 animate-pulse"></div>
             <div className="relative flex z-20 h-96">
                 {milestones.map((milestone, index) => (
                 <div key={milestone.yearLabel} className="relative flex-1 animate-fade-in-up cursor-default" style={{ animationDelay: `${index * 200}ms` }}>
                     <div className={
-                        `absolute left-1/2 transform -translate-x-1/2 w-[90%]
+                        `absolute left-1/2 transform -translate-x-1/2 w-[100%]
                         ${index % 2 === 0 ? 'bottom-1/2 mb-6' : 'top-1/2 mt-6'}
                         p-6 bg-white/90 backdrop-blur-sm rounded-xl shadow-xl border border-white/20
                         text-center transition-all duration-300 
@@ -52,12 +52,12 @@ export const TimeLine = () => {
             </div>
         </div>
 
-        <div className="grid md:hidden relative py-30">
-            <div className="absolute top-4 ml-15 bottom-2 w-1 bg-gradient-to-b from-white to-gray-400 rounded-full transform -translate-x-1/2 z-10 animate-pulse"></div>
-            <div className="grid z-20 h-200 grid-cols-1 gap-50 w-screen">
+        <div className="grid lg:hidden relative py-30 md:py-20">
+            <div className="absolute top-4 ml-15 md:ml-25 bottom-2 w-1 bg-gradient-to-b from-white to-gray-400 rounded-full transform -translate-x-1/2 z-10 animate-pulse"></div>
+            <div className="grid z-20 h-200 md:h-160 grid-cols-1 gap-50 md:gap-30 w-screen">
                 {milestones.map((milestone, index) => (
                 <div key={milestone.yearLabel} className="flex items-center animate-fade-in-up cursor-default" style={{ animationDelay: `${index * 200}ms` }}>
-                    <div className="absolute ml-50 transform -translate-x-1/2 w-[60%] p-3 bg-white/90 backdrop-blur-sm rounded-xl shadow-xl border border-white/20 text-center transition-all duration-300">
+                    <div className="absolute ml-50 md:ml-95 transform -translate-x-1/2 w-[60%] p-3 bg-white/90 backdrop-blur-sm rounded-xl shadow-xl border border-white/20 text-center transition-all duration-300">
                         <div className="absolute top-1/2 left-0 transform -translate-y-1/2 -translate-x-full border-e-[10px] border-t-[10px] border-b-[10px] border-t-transparent border-b-transparent border-e-white"></div>
                         <div className="text-lg font-semibold text-gray-800 mb-2 leading-tight">
                             {milestone.titleLabel}
@@ -67,13 +67,13 @@ export const TimeLine = () => {
                         </div>
                     </div>
                     
-                    <div className="absolute transform -translate-x-1/2 ml-15">
+                    <div className="absolute transform -translate-x-1/2 ml-15 md:ml-25">
                         <div className="w-6 h-6 bg-gray-800 rounded-full border-4 border-white shadow-lg transform transition-all duration-300 hover:scale-125 hover:shadow-xl hover:shadow-blue-500/30 cursor-pointer z-30">
                             <div className="absolute top-1/2 left-1/2 w-2 h-2 bg-white/80 rounded-full transform -translate-x-1/2 -translate-y-1/2"></div>
                         </div>
                     </div>
 
-                    <div className="absolute transform -translate-x-1/2 ml-3 text-xl font-bold text-gray-200 bg-clip-text tracking-wide">
+                    <div className="absolute transform -translate-x-1/2 ml-3 md:ml-12 text-xl font-bold text-gray-200 bg-clip-text tracking-wide">
                         {milestone.yearLabel}
                     </div>
                 </div>
