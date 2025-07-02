5e6937408c41df3497dc9da4f5b401cd0c0d5be0
Michalis Giannoudis <michalis221@gmail.com>
2025-07-02 15:35:18 +0300

    removed background from services options

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -69,7 +69,7 @@ export default function Header() {
       <div
       className="grid grid-cols-[72%_13%_15%] md:grid-cols-[20%_50%_20%_10%] lg:grid-cols-[34%_30%_21%_15%] text-center pointer-events-auto bg-gray-900/60 md:bg-gray-900/40 w-[95%] h-auto rounded-2xl md:rounded-4xl shadow-lg backdrop-blur-md drop-shadow-xl mt-5 absolute">
       <div className="relative flex items-center justify-start ml-3 md:ml-2">
-        <div className="absolute bg-slate-900/30 md:rounded-3xl md:w-34 lg:w-48 md:h-16 z-10 "/>
+        <div className="absolute bg-slate-900/30 md:rounded-3xl lg:ml-1 md:w-34 lg:w-48 md:h-16 z-10 "/>
         <img src="/logo-cropped.png" className="relative w-30 h-16 md:w-50 md:h-20 z-20 object-contain"/>
       </div>
       <div className="hidden md:grid grid-cols-3 justify-items-center items-center gap-2">
--- src/components/serviceCard.component.tsx
+++ src/components/serviceCard.component.tsx
@@ -1,12 +1,13 @@
 export const ServiceCard = ({ serviceImage, serviceLabel, serviceBody }: { serviceImage:string,serviceLabel: string, serviceBody:string }) => {
     return(
-        <div className="bg-white rounded-lg shadow-lg p-8 text-center hover:shadow-2xl transition-shadow duration-300">
-            <img src={serviceImage} className="w-16 h-16 bg-slate-200 rounded-lg flex items-center justify-center mx-auto mb-6 p-3"/>
-            <h3 className="text-xl h-14 font-semibold text-slate-800 mb-4 cursor-default">{serviceLabel}</h3>
-            <div className="flex flex-col gap-2 mb-6 text-left items-start">
+        <div className="bg-white flex flex-col rounded-lg shadow-lg p-8 text-center hover:shadow-2xl transition-shadow duration-300">
+            <img src={serviceImage} className="w-16 h-16 bg-slate-200 rounded-lg flex items-center justify-center mx-auto p-3 mb-4"/>
+            <h3 className="text-xl h-5 font-semibold text-slate-800 cursor-default mb-14 mt-3">{serviceLabel}</h3>
+            <div className="flex flex-col gap-4 text-left items-start">
                 {serviceBody.split(',').map((option, idx) => (
-                    <div key={idx} className="bg-slate-100 text-slate-800 px-3 py-1 rounded-full text-sm font-medium shadow hover:bg-slate-200 transition-colors cursor-default">
-                        { option.trim() }
+                    <div key={idx} className="flex items-start text-slate-800 text-sm font-medium">
+                        <span className="w-1.5 h-1.5 bg-slate-700 rounded-full mr-5 inline-block mt-1.5 flex-shrink-0" />
+                        <span>{option.trim()}</span>
                     </div>
                 ))}
             </div>
