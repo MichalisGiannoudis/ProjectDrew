0d2af03fb9bcb6d27a2a6544c60b8c04034e840e
Michalis Giannoudis <michalis221@gmail.com>
2025-07-08 14:00:32 +0300

    updated services content

--- src/components/mainBody.component.tsx
+++ src/components/mainBody.component.tsx
@@ -13,8 +13,8 @@ export default function MainBody() {
   
   const languageState = useThemeStore(state => state.language);
   const {  nameLabel, progessionLabel, bioHeaderLabel, bioBodyLabel, servicesLabel
-          , serviceLabel1, serviceLabel2, serviceLabel3, serviceLabel4, serviceBody1
-          , serviceBody2, serviceBody3, serviceBody4
+          , serviceLabel1, serviceLabel2, serviceLabel3, serviceLabel4, serviceLabel5, serviceLabel6, serviceLabel7
+          , serviceBody1, serviceBody2, serviceBody3, serviceBody4, serviceBody5, serviceBody6, serviceBody7
         } = useContent(languageState === Language.Greek ? 'mainPageGR' : 'mainPageEN') as MainPageContent;
   
   return (
@@ -62,11 +62,11 @@ export default function MainBody() {
             </div>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-16 md:gap-12 lg:gap-8">
-            {[1,2,3,4].map((_, idx) => (
+            {[1, 2, 3, 4, 5, 6, 7].map((_, idx) => (
               <ServiceCard key={ idx }
               serviceImage = { idx ===0 ? '/corporate-icon.png' : idx === 1 ? '/civil-icon.png' : idx === 2 ? '/policy-icon.png' : '/contract-icon.png' }
-              serviceLabel = { idx === 0 ? serviceLabel1 : idx === 1 ? serviceLabel2 : idx === 2 ? serviceLabel3 : serviceLabel4 }
-              serviceBody = { idx === 0 ? serviceBody1 : idx === 1 ? serviceBody2 : idx === 2 ? serviceBody3 : serviceBody4 }
+              serviceLabel = { idx === 0 ? serviceLabel1 : idx === 1 ? serviceLabel2 : idx === 2 ? serviceLabel3 : idx === 3 ? serviceLabel4 : idx === 4 ? serviceLabel5 : idx === 5 ? serviceLabel6 : serviceLabel7 }
+              serviceBody = { idx === 0 ? serviceBody1 : idx === 1 ? serviceBody2 : idx === 2 ? serviceBody3 :  idx === 3 ? serviceBody4 : idx === 4 ? serviceBody5 : idx === 5 ? serviceBody6 : serviceBody7 }
               />
             ))}
           </div>
